992e7229fad14249ed43e366dd6e24d96e1ca7df
Vu Anh Lam <vuanhlam000@gmail.com>
2023-09-17 20:28:45 +0700

    number,string,boolean types

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-console.log('ahihi....')
+function add(
+  n1: number,
+  n2: number,
+  printResult: boolean,
+  resultPharse: string
+) {
+  let result = n1 + n2;
+  if (printResult) {
+    console.log(resultPharse + result);
+  } else {
+    return result;
+  }
+}
+
+const num1 = 1;
+const num2 = 2;
+const printResult = true;
+const resultPharse = "Result is: ";
+
+add(num1, num2, printResult, resultPharse);
