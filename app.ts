6a612e51ca588124f29316e9704bcc7517550353
Vu Anh Lam <vuanhlam000@gmail.com>
2023-09-17 21:22:50 +0700

    Array Type

--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 const person: {
     name: string,
     age: number,
-    address: string
+    address: string,
+    hobbies: string[]
 } = {
     name: 'vu anh lam',
     age: 12,
-    address: 'viet nam'
+    address: 'viet nam',
+    hobbies: ['Sport', 'Cooking']
 }
 
-console.log(person.address)
-console.log(person.age)
-console.log(person.name)
+console.log(person.hobbies)
+
+let favorites: string[];
+favorites = 7
+favorites = ''
+favorites = ['ahihi', 'meme']
+
+let favorite1: any[]
+favorite1 = [1, 'ahihi']
